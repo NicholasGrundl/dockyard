165b651e95630cf1bb4d9369ebe614b064a5520d
Nicholas Grundl <nicholasgrundl@Nicholass-MacBook-Pro.local>
2024-11-15 19:11:20 -0800

    update dockerfile

--- src/dockyard/__init__.py
+++ src/dockyard/__init__.py
@@ -1,2 +1,9 @@
+"""Python package for Backend API
+"""
 
-__version__ = "0.1.1"
+# Package meta data (for publishing)
+__version__ = (0, 0, 1)
+__author__ = "Nick Grundl"
+__author_email__ = '"Nick Grundl" <nicholasgrundl@gmail.com>'
+__maintainer__ = "Nick Grundl"
+__maintainer_email__ = '"Nick Grundl" <nicholasgrundl@gmail.com>'
