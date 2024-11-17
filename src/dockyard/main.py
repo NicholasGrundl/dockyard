3014c2dd36bce55cd5f0356cdf888b6ed6a55c53
Nicholas Grundl <nicholasgrundl@gmail.com>
2024-11-16 20:18:37 -0800

    feat: fixing to relative api endpoints. let nginx configure the prefix

--- src/dockyard/main.py
+++ src/dockyard/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/api/health")
+@app.get("/health")
 async def health_check() -> dict[str, str]:
     return {
         "service" : "dockyard",
@@ -10,11 +11,11 @@ async def health_check() -> dict[str, str]:
     }
 
 
-@app.get("/api")
+@app.get("/")
 async def root()-> dict[str, str]:
-    return {"status": "ok"}
+    return HTMLResponse(content="<h1>Welcome to Dockyard</h1>", status_code=200)
 
 
-@app.get("/api/v1/{username}")
+@app.get("/v1/{username}")
 async def greet_user(username: str):
     return {"message": f"Hello {username}"}
